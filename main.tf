0a467be29685521353d1bd439365fb44863cb411
derek <derek@morethancertified.com>
2021-12-07 11:39:35 +0100

    Managed by Terraform

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ module "compute" {
   version = "1.0.0"
 
   aws_region = "us-east-1"
-  public_key_material = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDPKBOe4oY2kWojeRcaJgXVxiI8nv74j3852hbKTrOfTxEMTKKsqIpAaZs5kQTPRneoAUsvlPuBPQY0TBMXuOt+KpwCfn0vuZrX8GeS3b8YUtZ3dzGn6SHETc9/Cf3AqfazRfcL5kqnchJO4a6lVX394dBNjnNiNjdgs7smrCyFYsudpcTnlUNitRFla2G2Xv9N+H4k7cCGhyp0RMrHPhE/g8UrmlpwU/EeRqIrozHbFSeiqulyYSX2OUkV8Kc6qoi/n4QGRh07tY/UGhQL+RkJoAZiNuh06KPy+OIt+pbh5swUDcF1wEexVG7qEhKbrS971V85uwzXu6VzqzOg3n7H ubuntu@ip-172-31-9-199"
+  public_key_material = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDucikWToiu14QSt34G6X3vMnXOAyxS7kLPF/PtYXSNM81yH8V1eQ4RgbxBJLIwq3HsNQhhfaRiy9tgzzeSHvGlwfWnRsU0RbGazGgZJ/dzBEpQc4K5JINxUBG+U3Wy2FGubHLwoWNQ4N/k4hD58YJwJ5lbqVCbb1PzbP2W8HZaf1IMFWddwkRSIQDZqPS6gRg851CdmMq036vrVx/G7Ug4hoatuF+s1lRsi2Jn0aGE1zCfjXFpLU9BNdthzuzAYv1bt84G0CCX564+v2Pcyu+wouvsLLtZIgewJsIx71qQ6SrGLmtWR+O6dz4sImINLZzFhRLm51WiktpgqcjkrGQ1 ubuntu@ip-172-31-9-199"
   public_sg = "${module.networking.public_sg}"
   public_subnets = "${module.networking.public_subnets}"
 }
